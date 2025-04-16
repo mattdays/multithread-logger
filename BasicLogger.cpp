8ee9b03bc632cdc9c6b60055706a1bb82f52c360
Matthew Days <daysmatthew@gmail.com>
2025-04-16 14:21:49 -0400

    Add BasicLogger

---
+++ BasicLogger.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+class BasicLogger {
+public:
+    BasicLogger(const std::string& filename) {
+        logfile_.open(filename, std::ios::app);
+    }
+
+    ~BasicLogger() {
+        logfile_.close();
+    }
+
+    void log(const std::string& message) {
+        std::lock_guard<std::mutex> lock(log_mutex_);
+        logfile_ << message << std::endl;
+    }
+
+private:
+    std::ofstream logfile_;
+    std::mutex log_mutex_;
+};
+
+int main() {
+    BasicLogger basicLogger("log.txt");
+
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < 4; ++i) {
+        threads.emplace_back([&basicLogger, i]() {
+            for (int j = 0; j < 10; ++j) {
+                basicLogger.log("Thread " + std::to_string(i) + " log entry " + std::to_string(j));
+            }
+        });
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    std::cout << "Logging done. Check 'log.txt'." << std::endl;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
+
 # multithread-logger
-A multithreaded logger in C++ intended as a refresher exercise
+
+A multithreaded logger in C++ intended as a refresher exercise.
+
+## Running Basic Logger
+
+ 1. Compile it with `g++` or your favorite C++ compiler
+`g++ -std=c++11 BasicLogger.cpp -o BasicLogger`
+ 2. Run it!
+`./BasicLogger`
+ 3. Check the output!
+`Thread 0 log entry 0
+Thread 0 log entry 1
+...
+Thread 3 log entry 9`
+Each line is written safely, without overlapping thanks to the `std::mutex`.
